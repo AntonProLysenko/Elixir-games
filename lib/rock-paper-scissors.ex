a9d513c746b3561e5507fd9c045795da2d710b28
boldPro <probold777@gmail.com>
2023-10-23 19:52:55 -0400

    finish Games: Rock Paper Scissors exercise

---
+++ lib/rock-paper-scissors.ex
@@ -0,0 +1,23 @@
+defmodule Games.RockPaperScissors do
+  @variants ["rock", "paper", "scissors"]
+
+  def play do
+    ai_choise = Enum.random(@variants)
+    player_choise = String.trim(IO.gets("Choose rock, paper, or scissors: "))
+
+    IO.inspect(ai_choise, label: "AI chose");
+
+    case {ai_choise,player_choise} do
+
+      {"rock", "paper"} -> IO.puts("#{IO.ANSI.green()}You win!\n #{IO.ANSI.default_color()}#{player_choise} beats #{ai_choise}.")
+      {"paper", "scissors"}-> IO.puts("#{IO.ANSI.green()}You win!\n #{IO.ANSI.default_color()}#{player_choise} beats #{ai_choise}.")
+      {"scissors", "rock"}-> IO.puts("#{IO.ANSI.green()}You win!\n #{IO.ANSI.default_color()}#{player_choise} beats #{ai_choise}.")
+
+      {"paper", "rock"}-> IO.puts("#{IO.ANSI.red()}You lose!\n #{IO.ANSI.default_color} #{ai_choise} beats #{player_choise}.")
+      {"scissors", "paper"}-> IO.puts("#{IO.ANSI.red()}You lose!\n #{IO.ANSI.default_color} #{ai_choise} beats #{player_choise}.")
+      {"rock", "scissors"}-> IO.puts("#{IO.ANSI.red()}You lose!\n #{IO.ANSI.default_color} #{ai_choise} beats #{player_choise}.")
+       {_, _}-> IO.puts("#{IO.ANSI.blue()}Draw!")
+    end
+
+  end
+end
