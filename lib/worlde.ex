d4cc6c25465ed3526ae306c448b275cc59597e0e
boldPro <probold777@gmail.com>
2023-10-24 21:56:04 -0400

    Running over two words and comparing each character with no result so far

--- lib/worlde.ex
+++ lib/worlde.ex
@@ -3,8 +3,6 @@ defmodule Games.Worlde do
     def get_user_data do
         answer = IO.gets("gues a word of 5 letters: ")
 
-        IO.puts(String.length(answer))
-
         if String.length(answer) < 6 || String.length(answer) > 6 do
             IO.puts("Enter a string exactly of 5 letters! \n")
             get_user_data()
@@ -13,16 +11,45 @@ defmodule Games.Worlde do
         end
 
     end
-    def generate_string do
-      Enum.map(1..5, fn _ -> Enum.random(?a..?z) end)
-    end
-    # def feedback answer, guess  do
 
-    # end
+
+    def generate_string do
+        Enum.map(1..5, fn _ ->
+            Enum.random(?a..?z)
+        end)
+    end
+
+
+
+    def feedback(answer,guess)  do
+        IO.inspect(answer, label: "answ")
+        IO.inspect(guess, label: "guess")
+
+        compare(answer, guess)
+    end
+
+
+
+    def compare(answer, guess) do
+
+        for a <- answer, g <- guess do
+            cond do
+              a == g -> IO.puts(:yelow)
+              true -> IO.puts("none")
+            end
+            {a, g}
+        end
+
+
+    end
 
 
     def play do
-        guess = get_user_data()
+        guess = String.to_charlist(String.slice(get_user_data(), 0..4))
+
         answer = generate_string()
+
+        feedback(answer, guess)
+
     end
 end
