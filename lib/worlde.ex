a756290f5d4bcd625df8005b2dcba5e1ea7c9538
boldPro <probold777@gmail.com>
2023-10-26 14:05:31 -0400

    returns correct list of color atoms

--- lib/worlde.ex
+++ lib/worlde.ex
@@ -32,6 +32,14 @@ defmodule Games.Worlde do
 
     def compare(answer, guess) do
 
+        same_chars_indexes =
+            Enum.map(guess, fn g ->
+                if g in answer do
+                    index = elem(Enum.with_index(answer)|> Enum.find(fn {char, index} -> char == g end),1)
+                end
+            end)
+            |>Enum.filter(& !is_nil(&1))
+
         chunked =
             for a <- answer, g <- guess do
                 {a, g}
@@ -39,21 +47,56 @@ defmodule Games.Worlde do
             |>Enum.chunk_every(5)
             |>Enum.with_index()
 
-        Enum.map(chunked, fn char_comparison->
-            index = elem(char_comparison, 1)
-            comparison = elem(char_comparison, 0)
-            Enum.at(comparison,index)
-        end)
-        |>
-        Enum.map(fn two_chars->
-            if elem(two_chars, 0) === elem(two_chars, 1) do
-              :green
+
+        color_matching_list =
+            Enum.map(chunked, fn char_comparison->
+                index = elem(char_comparison, 1)
+                comparison = elem(char_comparison, 0)
+                Enum.at(comparison,index)
+            end)
+            |>
+            Enum.map(fn two_chars->
+                if elem(two_chars, 0) === elem(two_chars, 1) do
+                    :green
+                else
+                    :gray
+                end
+            end)
+
+
+
+
+
+        # Enum.map(same_chars_indexes, fn index ->
+            # if Enum.at(color_matching_list, index) == :gray do
+            #    color_matching_list = List.update_at(color_matching_list, index, fn _ -> :yellow end)
+            # else
+            #    color_matching_list
+            # end
+        # end)
+
+        # for i<- same_chars_indexes, Enum.at(color_matching_list, i) == :gray do
+        #     color_matching_list = List.update_at(color_matching_list, i, fn _color-> :yelow end)
+        # end
+
+        new_value = :yelow
+
+        values = color_matching_list #List.duplicate(0, 5) # [0, 0, 0, 0, 0] in nicer form :-)
+        indexes = same_chars_indexes
+
+        values
+        |> Enum.with_index
+        |> Enum.map(fn {value, index} ->
+            if index in indexes and value != :green do
+                new_value
             else
-                :gray
+                value
             end
         end)
 
     end
+
+
 
 
     def play do
