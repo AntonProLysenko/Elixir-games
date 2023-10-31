b6edd692444062b6af0aafefc3eef2347b0e49e0
boldPro <probold777@gmail.com>
2023-10-31 09:07:43 -0400

    alg

--- lib/worlde.ex
+++ lib/worlde.ex
@@ -19,8 +19,6 @@ defmodule Games.Worlde do
         end)
     end
 
-
-
     def feedback(answer,guess)  do
         IO.inspect(answer, label: "answe")
         IO.inspect(guess, label: "guess")
@@ -29,27 +27,61 @@ defmodule Games.Worlde do
     end
 
 
+    # def find_same_char_indexes([],answer), do: nil
+
+    # def find_same_char_indexes([ ghd | gtl ], answer)do
+    #     answer = [head|tail]
+
+    #     return = []
+    #     # counter = 0
+    #     index_answer =
+
+    #     if elem(ghd,0) in answer && !(elem(ghd,1) in return)  do
+    #         # counter=counter+1
+    #         return = [elem(ghd,1)|return]
+    #         find_same_char_indexes(gtl, answer)
+    #     else
+    #         find_same_char_indexes(gtl, answer)
+    #     end
+
+    #     return
+
+    # end
+
+
+
 
     def compare(answer, guess) do
+        indexed_answer = Enum.with_index(answer)
 
         same_chars_indexes =
             Enum.map(guess, fn g ->
-                if g in answer do
-                    index = elem(Enum.with_index(answer)|> Enum.find(fn {char, index} -> char == g end),1)
+                if g in answer  do
+                    Enum.find(indexed_answer, fn {char, _index} ->
+                         char == g end)
+                    |>elem(0)
+                end
+            end)
+            |>Enum.with_index()
+            |> Enum.map(fn el ->
+                if elem(el,0) !== nil do
+                    elem(el,1)
                 end
             end)
             |>Enum.filter(& !is_nil(&1))
+            IO.inspect(same_chars_indexes, label: "Indexes")
+
 
         chunked =
-            for a <- answer, g <- guess do
+            for a <- answer,  g <- guess do
                 {a, g}
             end
             |>Enum.chunk_every(5)
-            |>Enum.with_index()
+            # |>IO.inspect(label: "Whole")
 
 
         color_matching_list =
-            Enum.map(chunked, fn char_comparison->
+            Enum.map(chunked|>Enum.with_index(), fn char_comparison->
                 index = elem(char_comparison, 1)
                 comparison = elem(char_comparison, 0)
                 Enum.at(comparison,index)
@@ -63,47 +95,45 @@ defmodule Games.Worlde do
                 end
             end)
 
+        # test_index_array =
+        #     Enum.map(chunked, fn row->
+        #         Enum.with_index(row)
+        #         |>
+        #         Enum.map(fn{char_pair,index} ->
+        #             if elem(char_pair,0) == elem(char_pair,1) do
+        #                 index
+        #             end
+        #         end)
+        #     end)
+        #     |>Enum.with_index()
+        #     |>IO.inspect()
+        #     |> Enum.map(fn {row,index}->
+        #         Enum.at(row,index+1)
+        #     end)
+        #     |>Enum.filter(& !is_nil(&1))
+        #     IO.inspect(test_index_array, label: "Test Indexes")
 
 
-
-
-        # Enum.map(same_chars_indexes, fn index ->
-            # if Enum.at(color_matching_list, index) == :gray do
-            #    color_matching_list = List.update_at(color_matching_list, index, fn _ -> :yellow end)
-            # else
-            #    color_matching_list
-            # end
-        # end)
-
-        # for i<- same_chars_indexes, Enum.at(color_matching_list, i) == :gray do
-        #     color_matching_list = List.update_at(color_matching_list, i, fn _color-> :yelow end)
-        # end
-
-        new_value = :yelow
-
-        values = color_matching_list #List.duplicate(0, 5) # [0, 0, 0, 0, 0] in nicer form :-)
-        indexes = same_chars_indexes
-
-        values
-        |> Enum.with_index
-        |> Enum.map(fn {value, index} ->
-            if index in indexes and value != :green do
-                new_value
+         Enum.with_index(color_matching_list)
+        |> Enum.map(fn {color, index} ->
+            if index in same_chars_indexes and color != :green and color != :yelow do
+                :yelow
             else
-                value
+                color
             end
         end)
+
+
 
     end
 
 
 
-
     def play do
         guess = String.to_charlist(String.slice(get_user_data(), 0..4))
-
         # answer = generate_string()
-        answer = ~c"asdfg"
+        answer = ~c"aaabb"
+        # answer = ~c"asdfg"
 
         feedback(answer, guess)
 
