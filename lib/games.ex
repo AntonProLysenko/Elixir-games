256e76bb549ea856c9bd7b8e64657118c5a9a93b
boldPro <probold777@gmail.com>
2023-11-02 13:23:00 -0400

    added menu to launch games

--- lib/games.ex
+++ lib/games.ex
@@ -12,7 +12,48 @@ defmodule Games do
       :world
 
   """
-  def hello do
-    :world
+
+  def main(_args) do
+    player_input = IO.gets(
+      "What game would you like to play?
+        1. Guessing Game
+        2. Rock Paper Scissors
+        3. Wordle
+
+        enter 'stop' to exit: \n")
+
+    IO.inspect(player_input, label: "Input")
+    game =
+      case player_input do
+        "1\n" -> elem(Integer.parse(player_input),0)
+        "2\n" -> elem(Integer.parse(player_input),0)
+        "3\n" -> elem(Integer.parse(player_input),0)
+        "stop\n" -> player_input
+      end
+
+    case game do
+      1 -> play(["--game=1"])
+      2 -> play(["--game=2"])
+      3 -> play(["--game=3"])
+      "stop\n"  -> IO.puts("Stoped!")
+    end
+
   end
+
+
+  def play(args) do
+
+    {opts, _word, _error} = OptionParser.parse(args, switches: [game: :integer])
+
+    case opts[:game] do
+      1 -> Games.GuessingGame.play()
+      2 -> Games.RockPaperScissors.play()
+      3 -> Games.Worlde.play()
+      _ -> IO.inspect("Error #{opts[:game]}")
+    end
+
+
+
+  end
+
 end
