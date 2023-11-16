9a499dc0154469b0a9763aae6ffa29c8ba1d3b5c
boldPro <probold777@gmail.com>
2023-11-16 11:21:32 -0500

    Final

--- lib/worlde.ex
+++ lib/worlde.ex
@@ -15,8 +15,7 @@ defmodule Games.Worlde do
   """
 
   def get_user_data do
-    guess = IO.gets("Guess a five-letter word: ") |> IO.inspect(label: "Guess")
-
+    guess = IO.gets("Guess a five-letter word: ")
     cond do
       guess == "stop\n" ->
         Games.main(1)
@@ -41,10 +40,10 @@ defmodule Games.Worlde do
   #     ~c"asdfg"
   """
   def generate_string do
-    # Enum.map(1..5, fn _ ->
-    #     Enum.random(?a..?z)
-    # end)
-    ~c"aaabb"
+    Enum.map(1..5, fn _ ->
+        Enum.random(?a..?z)
+    end)
+    # ~c"aaabb"
   end
 
   @doc """
@@ -90,121 +89,36 @@ defmodule Games.Worlde do
     guess_occurrences > answer_occurrences
   end
 
-  defp find_idxs(guess, answer, return \\ [])
+  # defp find_idxs(guess, answer, return \\ [])
 
-  defp find_idxs([], _, return), do: return
+  # defp find_idxs([], _, return), do: return
 
-  # defp find_idxs([_ahd | atl], answer, return) when length(answer)==0, do: find_idxs(atl, answer, return)
-  # defp find_idxs(guess,[] = answer, return), do: find_idxs(guess, answer, return )
-  defp find_idxs(_guess, [], return), do: return
+  # # defp find_idxs([_ahd | atl], answer, return) when length(answer)==0, do: find_idxs(atl, answer, return)
+  # # defp find_idxs(guess,[] = answer, return), do: find_idxs(guess, answer, return )
+  # defp find_idxs(_guess, [], return), do: return
 
-  defp find_idxs([ghd | gtl], answer = [ahd | atl], return) do
-    IO.inspect([ahd | atl], label: "indexed answ")
-    IO.inspect(ahd, label: "compared AHD")
-    IO.inspect(ghd, label: "compared G")
-    IO.inspect(return, label: "RETURN")
-    IO.inspect(length(answer), label: "length ANSW")
+  # defp find_idxs([ghd | gtl], answer = [ahd | atl], return) do
 
-    # if answer == [] do
-    #   find_idxs(gtl, [ahd| atl], return)
-    # end
 
-    cond do
-      elem(ahd, 0) == ghd ->
-        IO.puts("MATCH!!!")
-        IO.puts(elem(ahd, 1))
-        # elem(ahd,1)
-        find_idxs(gtl, atl, return ++ [elem(ahd, 1)])
+  #   # if answer == [] do
+  #   #   find_idxs(gtl, [ahd| atl], return)
+  #   # end
 
-      elem(ahd, 0) !== ghd ->
-        find_idxs([ghd | gtl], atl, return)
-    end
+  #   cond do
+  #     elem(ahd, 0) == ghd ->
+  #       IO.puts("MATCH!!!")
+  #       IO.puts(elem(ahd, 1))
+  #       # elem(ahd,1)
+  #       find_idxs(gtl, atl, return ++ [elem(ahd, 1)])
 
-    return
-  end
+  #     elem(ahd, 0) !== ghd ->
+  #       find_idxs([ghd | gtl], atl, return)
+  #   end
 
-  @doc """
-      main function of the game
-      compares two charlists and return a list of color-atoms
+  #   return
+  # end
 
-      color_matching_list is a return
-          colors in color_matching_list depends:
-              if the char in guess in a right place -> it replaces the char with a :green atom
-              if a char in guess not found in the answer -> it replaces with a :grey atom
-              if a char in guess exists in answer but placed in a wrong place -> it replaces with a :yellow atom
-      same_chars_indexes - is a var where we are saving all the matching chars's indexes
-      color_matching_list - is a var where :green and :grey atoms are assigned
-      and then  color_matching_list is getting merged with  same_chars_indexes excluding :green values
-  """
-  @spec compare(list(), list()) :: list()
-  def compare(answer, guess) do
-    same_chars_indexes =
-      Enum.map(guess, fn g ->
-        if g in answer do
-          if find_idxs(guess, Enum.with_index(answer)) == [] do
-            find_idxs(guess -- [hd(guess)], Enum.with_index(answer))
-          end
-        else
-          nil
-        end
-      end)
 
-    # |>Enum.with_index()
-    # |> Stream.map(fn {num, idx} ->
-    #     if num !== nil do
-    #         idx
-    #     end
-    # end)
-    # |>Enum.filter(& !is_nil(&1))
-    IO.inspect(same_chars_indexes, label: "IDXS")
-
-    chunked =
-      for a <- answer, g <- guess do
-        {a, g}
-      end
-      |> Enum.chunk_every(5)
-
-    color_matching_list =
-      Enum.map(Enum.with_index(chunked), fn char_comparison ->
-        index = elem(char_comparison, 1)
-        comparison = elem(char_comparison, 0)
-        Enum.at(comparison, index)
-      end)
-      |> Enum.map(fn two_chars ->
-        if elem(two_chars, 0) === elem(two_chars, 1) do
-          :green
-        else
-          :grey
-        end
-      end)
-
-    # test_index_array =
-    #     Enum.map(chunked, fn row->
-    #         Enum.with_index(row)
-    #         |>
-    #         Enum.map(fn{char_pair,index} ->
-    #             if elem(char_pair,0) == elem(char_pair,1) do
-    #                 index
-    #             end
-    #         end)
-    #     end)
-    #     |>Enum.with_index()
-    #     |>IO.inspect()
-    #     |> Enum.map(fn {row,index}->
-    #         Enum.at(row,index+1)
-    #     end)
-    #     |>Enum.filter(& !is_nil(&1))
-    #     IO.inspect(test_index_array, label: "Test Indexes")
-
-    Enum.with_index(color_matching_list)
-    |> Enum.map(fn {color, index} ->
-      if index in same_chars_indexes and color === :grey do
-        :yellow
-      else
-        color
-      end
-    end)
-  end
 
   @doc """
       function that manage and runs the whole game
@@ -253,6 +167,7 @@ defmodule Games.Worlde do
       end
     else
       IO.puts("#{IO.ANSI.red()}\nGame Over! #{IO.ANSI.default_color()}\n")
+      IO.puts("The Answer was:"<>"#{IO.ANSI.green()} #{to_string(answer)}")
       Games.main(1)
     end
   end
