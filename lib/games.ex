47285be609403b9d7e59528484335f2e18953ba7
boldPro <probold777@gmail.com>
2023-12-23 12:06:38 -0500

    found a way to track the score without pid comunication

--- lib/games.ex
+++ lib/games.ex
@@ -17,15 +17,15 @@ defmodule Games do
     #Calculates and stores a score
     use GenServer
     def start_link(arg\\0) do
-      GenServer.start_link(__MODULE__, arg)
+      GenServer.start_link(__MODULE__, arg, name: __MODULE__)
     end
 
-    def get_score(pid) do
-      GenServer.call(pid, :get_score)
+    def get_score(_pid) do
+      GenServer.call(__MODULE__, :get_score)
     end
 
-    def add_points(pid, points) do
-      GenServer.cast(pid, {:add_points, points})
+    def add_points(_pid, points) do
+      GenServer.cast(__MODULE__, {:add_points, points})
     end
 
     @impl true
@@ -57,7 +57,8 @@ defmodule Games do
     {:ok, pid}  =
       if state == [] do
         IO.puts("\n\n\n\n#{IO.ANSI.blue_background(); IO.ANSI.font_9()}-----======#{IO.ANSI.green()} Welcome #{IO.ANSI.red()}To Anton's #{IO.ANSI.yellow()}Game Pack! #{IO.ANSI.default_color()}======----- \n\n")
-        Score.start_link(0)
+        # Score.start_link(0)
+        {:ok, 1}
       else
         # IO.puts("Not starting State")
         {:ok, state}
